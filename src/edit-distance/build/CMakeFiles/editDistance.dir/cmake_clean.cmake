18c27fc1cac51383a0943f3674dc560d3b544c61
Tuan Tu Tran <tu.tt@financialdeepmind.com>
2023-02-26 16:50:04 -0500

    finish the first DP version

---
+++ src/edit-distance/build/CMakeFiles/editDistance.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/editDistance.dir/src/editDistance.cpp.o"
+  "editDistance"
+  "editDistance.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/editDistance.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/edit-distance/src/editDistance.cpp
+++ src/edit-distance/src/editDistance.cpp
@@ -2,19 +2,40 @@
 // find the Levenshtein distance between 2 text strings
 #include<iostream>
 #include<vector>
+#include<string>
 
 class EditDistance {
 public:
-    static int calculateLevenshteinDist(const string& str1, const string& str2){
-        int ans;
-        // Todo: implement with the dynamics programming approach
-        //
-        return ans;
+    static int calculateLevenshteinDist(const std::string& str1, const std::string& str2){
+        int nbRow = str1.length();
+        int nbCol = str2.length();
+        std::vector<std::vector<int>> dp(nbRow+1,std::vector<int>(nbCol+1));
+        // initialization
+        for(int row=0;row<=nbRow;row++) dp[row][0] = row;
+        for(int col=0;col<=nbCol;col++) dp[0][col] = col;
+        // calculate the dp matrix
+        for(int row=1;row<=nbRow;row++){
+            for(int col=1;col<=nbCol;col++){
+                if(str1[row-1] == str2[col-1]){
+                    dp[row][col] = dp[row-1][col-1];
+                }
+                else{
+                    int tmp = std::min(dp[row-1][col-1],dp[row][col-1]); // either substitution or insertion
+                    dp[row][col] = std::min(tmp,dp[row-1][col]) + 1;     // or deletion
+                }
+            }
+        }
+        // 
+        return dp[nbRow][nbCol];
     }
 };
 
 int main(int argc, char** argv){
-    // TODO: implement the algorithm
-
+    // TODO: add test module
+    // TODO: add trace-back
+    std::string str1("element");
+    std::string str2("evidence");
+    int editDistance = EditDistance::calculateLevenshteinDist(str1,str2);
+    std::cout << "Edit distance = " << editDistance << std::endl; 
     return 0;
 }
