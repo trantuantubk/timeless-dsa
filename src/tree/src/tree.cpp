b5459a6a13e2789da3b51511184bce4f2fafd0a7
Tuan Tu Tran <tu.tt@financialdeepmind.com>
2023-02-27 10:01:57 -0500

    add implementation of 2 methods of the class BinaryTree

--- src/tree/src/tree.cpp
+++ src/tree/src/tree.cpp
@@ -1,3 +1,10 @@
 #include "tree.h"
 // TODO: implement the general Tree class
 
+void BinaryTree::createTree(){
+    //TODO: implement the method to create the binary tree from an input array
+}
+
+void BinaryTree::displayTree(){
+    //TODO: implement the method to display the tree, by default, using in order traversal
+}
